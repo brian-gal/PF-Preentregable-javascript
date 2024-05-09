ddffd82db118be1fe5477b095e448a3df5b00eb9
brian <briamgalvez2013@gmail.com>
2024-05-09 07:11:48 -0300

    juego adivinanzas aleatorio terminado, falta optimizar

--- js/script.js
+++ js/script.js
@@ -1,59 +1,94 @@
-/* let instrucciones = alert("intrucciones\nEl juego consiste en adivinar un numero aleatorio, contando solo con la pista de los aciertos, es decir si el numero es 155 y escribis 160 tendrias 1 acierto ya que el 1 esta en la pocision correcta, caso contrario si pones 610 ya que no tendrias ningun acierto, utiliza esto a tu favor para descubrir cual es el numero magico.\nHaga click en aceptar para comenzar")
-*/
-let dificultad = prompt("elija la dificultad\nFacil: Numero de 3 cifras mas historial con \nDificil: Numero de 4 cifras mas historial\nExperto: Numero de 4 cifras sin historial")
-/* while (dificultad !== "facil" && dificultad !== "dificil" && dificultad !== "experto") {
-    dificultad = prompt("por favor, escriba una dificultad valida\nFacil\nDificil\nExperto")
-} */
-
-let numeroAleatorio = []
-if (dificultad == "facil") {
-    for (let i = 0; i < 3; i++) {
-        numeroAleatorio.push(Math.floor(Math.random() * 10));
-    }
-}
-else {
-    for (let i = 0; i < 4; i++) {
-        numeroAleatorio.push(Math.floor(Math.random() * 10));
-    }
-}
-
+let dificultad;
 let resultadoAnalizar = []
 let historial = []
+let numeroAleatorio = []
 let resultado = 0
 let aciertos = 0
 let maxIntentos = 20
 let intentos = 0
+let desde;
+let hasta;
 
-while (intentos < maxIntentos && aciertos < numeroAleatorio.length && resultado != "me rindo") {
-    resultado = prompt("Escriba un numero de " + numeroAleatorio.length + " cifras" + "\nHISTORIAL: " + historial.join(' - ') + "\nIntentos disponibles: " + (maxIntentos - intentos) + "\n Aciertos: " + aciertos);
-    resultadoAnalizar = []
-    resultadoAnalizar.push(...resultado);
-    historial.push(resultado);
-    intentos++
+function estableceDificultad() {
+    while (true) {
+        dificultad = parseInt(prompt("ELIJA LA DIFICULTAD:\n\n2 = Facil: del 10 al 99\n3 = Medio: del 100 al 9993\n4 = Dificil: del 1000 al 9999"));
 
-    aciertos = 0
-    for (let i = 0; i < numeroAleatorio.length; i++) {
-        if (numeroAleatorio[i] == resultadoAnalizar[i]) {
-            aciertos++
+        if (dificultad === 2 || dificultad === 3 || dificultad === 4) {
+            break;
+        } else {
+            alert("Por favor, ingrese un valor válido (2, 3 o 4).");
+        }
+    }
+
+    switch (dificultad) {
+        case 2:
+            desde = 10
+            hasta = 99
+            break;
+        case 3:
+            desde = 100
+            hasta = 999
+            break;
+        case 4:
+            desde = 1000
+            hasta = 9999
+            maxIntentos = 30
+            break;
+        default:
+            break;
+    }
+}
+
+function creandoAleatorio() {
+    for (let i = 0; i < dificultad; i++) {
+        numeroAleatorio.push(Math.floor(Math.random() * 10));
+    }
+    if (numeroAleatorio[0] == 0) {
+        numeroAleatorio[0] = Math.floor(Math.random() * (9 - 1) + 1);
+    }
+}
+
+function Analisis() {
+    while (intentos < maxIntentos && aciertos < numeroAleatorio.length && resultado != "ME RINDO") {
+
+        while (true) {
+            resultado = prompt("ESCRIBA UN NUMERO DEL " + desde + " al " + hasta + "\n\nHistorial: " + historial.join(' - ') + "\nIntentos disponibles: " + (maxIntentos - intentos) + "\nAciertos: " + aciertos + "\nPara rendirse escribi ME RINDO");
+            if ((resultado >= desde && resultado <= hasta) || resultado == "ME RINDO") {
+                break;
+            } else {
+                alert("Por favor, ingrese un número válido de " + desde + " al " + hasta + ".");
+            }
+        }
+
+        resultadoAnalizar = [] // restablece a sin valor el array en cada vuelta
+        resultadoAnalizar.push(...resultado);
+        historial.push(resultado);
+        intentos++
+
+        aciertos = 0  // restablece a 0 en cada vuelta
+        for (let i = 0; i < numeroAleatorio.length; i++) {
+            if (numeroAleatorio[i] == resultadoAnalizar[i]) {
+                aciertos++
+            }
         }
     }
 }
 
-if (aciertos == numeroAleatorio.length) {
-    alert("¡Felicidades, ganaste! Descubriste el número " + numeroAleatorio.join(""));
-}
-if (resultado == "me rindo") {
-    alert("Nos entristese que te rindas. El número era " + numeroAleatorio.join("") + ". Espero verte pronto intentandolo de nuevo");
-}
-else {
-    alert("Lo siento, has agotado tus intentos. El número era " + numeroAleatorio.join(""));
+function ganador() {
+    if (aciertos == numeroAleatorio.length) {
+        alert("¡Felicidades GANASTE! Descubriste el número " + numeroAleatorio.join("") + " con " + intentos + " intentos.");
+    }
+    else if (resultado == "ME RINDO") {
+        alert("Me entristese que te des por vencido. El número era " + numeroAleatorio.join("") + ". Espero verte pronto, tal vez haya mas suerte la proxima.");
+    }
+    else {
+        alert("GAME OVER\nTe has quedado sin intentos. El número era " + numeroAleatorio.join("") + ". Espero verte pronto, tal vez haya mas suerte la proxima.");
+    }
 }
 
-console.log(numeroAleatorio)
-console.log(resultadoAnalizar)
-console.log(historial)
-console.log("aciertos " + aciertos)
-console.log("intentos " + intentos)
-console.log("numero ingresado " + resultado)
-console.log("maximos intentos " + maxIntentos)
-console.log("numero objetivo " + numeroAleatorio.join(""))
+estableceDificultad()
+creandoAleatorio()
+Analisis()
+ganador()
+
+
